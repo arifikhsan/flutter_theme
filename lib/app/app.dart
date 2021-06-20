a3f8aff5fa9cb412cfa7ac0d7ef375202099cd00
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-20 13:59:48 +0700

    theme data

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_theme/counter/counter.dart';
 import 'package:flutter_theme/l10n/l10n.dart';
+import 'package:flutter_theme/text/text_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -19,13 +19,27 @@ class App extends StatelessWidget {
       theme: ThemeData(
         accentColor: const Color(0xFF13B9FF),
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        // brightness: Brightness.dark,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+          headline2: TextStyle(
+            fontSize: 54,
+            fontWeight: FontWeight.bold,
+          ),
+          headline3: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(fontSize: 16),
+          bodyText2: TextStyle(fontSize: 16),
+        ),
       ),
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const TextPage(),
     );
   }
 }
